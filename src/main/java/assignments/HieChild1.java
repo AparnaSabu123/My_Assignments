3ce0190fdadc3054313516fc58cd2e952f9f7b8d
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-07-28 07:51:43 +0530

    hierarchy child 1 push

---
+++ src/main/java/assignments/HieChild1.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class HieChild1 extends HieParent{
+
+	public static void main(String[] args) {
+		
+		HieChild1 hc=new HieChild1();
+		hc.display();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
