ca1cd748da3470bd4f5680c6457bf3e42dd54e2a
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-07-28 07:52:01 +0530

    hierarchy child 2 push

---
+++ src/main/java/assignments/HieChild2.java
@@ -0,0 +1,14 @@
+package assignments;
+
+public class HieChild2 extends HieParent {
+
+	public static void main(String[] args) {
+		
+		HieChild2 hc=new HieChild2();
+		hc.display();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
