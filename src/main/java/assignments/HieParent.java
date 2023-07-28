4a20b6dd5236537bc4489ec1d7803ef839ee35af
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-07-28 07:51:12 +0530

    Hierarchy parent push

---
+++ src/main/java/assignments/HieParent.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class HieParent {
+	
+	int num=100;
+	public void display()
+	{
+		String empName="Athira";
+		int employeeId=30;
+		System.out.println(empName);
+		System.out.println(employeeId);
+		
+	}
+
+}
