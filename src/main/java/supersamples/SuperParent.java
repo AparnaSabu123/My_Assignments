c654cd8483a78cae68cbbee57550d6c39215c519
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-08-01 07:35:14 +0530

    super parent commit

---
+++ src/main/java/supersamples/SuperParent.java
@@ -0,0 +1,13 @@
+package supersamples;
+
+public class SuperParent {
+	int c=100;
+	public void empDetails()
+	{
+		int empid=1234;
+		String empName="Aparna";
+		System.out.println(empid);
+		System.out.println(empName);
+	}
+
+}
