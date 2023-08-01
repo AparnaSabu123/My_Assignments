720cd2c607f884d51c75d26167f088b9038b4290
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-08-01 07:35:57 +0530

    super parent constructor commit and push

---
+++ src/main/java/supersamples/SuperParentCons.java
@@ -0,0 +1,13 @@
+package supersamples;
+
+public class SuperParentCons {
+	
+	public SuperParentCons()
+	{
+		String name="Aparna";
+		int age=30;
+		System.out.println(name);
+		System.out.println(age);
+	}
+
+}
