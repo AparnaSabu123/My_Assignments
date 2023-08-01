bf2423a4a57ccfcd4f55921434bbdb752a0350c9
Aparna <Aparna@LAPTOP-5AU7GJHM>
2023-08-01 07:36:15 +0530

    super child constructor commit and push

---
+++ src/main/java/supersamples/SuperChildCons.java
@@ -0,0 +1,18 @@
+package supersamples;
+
+public class SuperChildCons extends SuperParentCons {
+	
+	public SuperChildCons()
+	{
+		super();
+	}
+
+	public static void main(String[] args) {
+		
+		SuperChildCons sc=new SuperChildCons();
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
